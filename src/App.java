3d852ee41e03b93881820e064eb1850c37b66614
wagnermarques <wagnerdocri@gmail.com>
2026-02-24 11:08:18 -0300

    Aula 24/02

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Usuario user = new Usuario();
+        Scanner scanner = new Scanner(System.in);  
+        System.out.print("Digite o nome do usuário: ");
+        user.nome = scanner.nextLine();
+        System.out.println("Hello, " + user.nome + "!");
+        scanner.close();
     }
 }
