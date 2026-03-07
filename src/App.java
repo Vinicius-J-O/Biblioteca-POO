ea9e3315a651c38cf5cc3b45bff18a0d8a0ec54e
Vinícius Jesus de Oliveira <viniciusjesusdeoliveirapro@gmail.com>
2026-03-07 17:42:07 -0300

    Cadastro

--- src/App.java
+++ src/App.java
@@ -1,34 +1,74 @@
-
 import java.util.Scanner;
+import java.util.ArrayList;
 
-public class App{
-   public static void main(String[] args) throws Exception {
-    Scanner scanner = new Scanner(System.in);
-    System.out.println("================");
-    System.out.println("=  Bem Vindo!  =");
-    System.out.println("================");
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        boolean rodando = true;
+        ArrayList<Usuario> usuarios = new ArrayList<>();
 
-    boolean rodando = true;
-    while (rodando) {
-        System.out.println("===================");
-        System.out.println("=       MENU      =");
-        System.out.println("= 1.Listar Acervo =");
-        System.out.println("=      0.Sair     =");
-        System.out.println("===================");
+        System.out.println("=================================");
+        System.out.println("==           Bem Vindo         ==");
+        System.out.println("=================================");
+        do {
+            System.out.println("==============================================");
+            System.out.println("==          1.Cadastrar                     ==");
+            System.out.println("==          2.Login                         ==");
+            System.out.println("==          3.Informações de login          ==");
+            System.out.println("==          0.Sair                          ==");
+            System.out.println("==============================================");
+            System.out.println("R:");
+            String resp = sc.nextLine().trim();
+
+            switch (resp) {
+                case "1":
+                    System.out.println("Digite o seu nome");
+                    String nome = sc.nextLine().trim();
+                    System.out.println("Digite o seu sobrenome");
+                    String sobrenome = sc.nextLine();
+                    String nomeCompleto = nome + " " + sobrenome;
+                    System.out.println("Digite a senha");
+                    String senha = sc.nextLine().trim();
+                    System.out.println("Digite o seu email");
+                    String email = sc.nextLine().trim();
+                    Usuario novoUsuario = new Usuario(nome, nomeCompleto, email, senha, sobrenome);
+                    usuarios.add(novoUsuario);
+                    System.out.println("Novo usuário cadastrado com sucesso! ID: " + novoUsuario.getId());
+                    break;
+                
+                case "2":
+                     if (usuarios.isEmpty()) {
+                        System.out.println("Nenhum usuário cadastrado ainda!");
+                        break;
+                    }
+
+                    System.out.println("Email:");
+                    String loginEmail = sc.nextLine();
+                    System.out.println("Senha:");
+                    String loginSenha = sc.nextLine();
+                    Usuario usuarioLogado = null;
+                     for (Usuario u : usuarios) {
+                        if (u.getEmail().equals(loginEmail) && u.verificarSenha(loginSenha)) {
+                            usuarioLogado = u;
+                            break;
+                        }
+                    }
+                     if (usuarioLogado != null) {
+                        System.out.println("Login realizado com sucesso!");
+                        System.out.println("Bem-vindo(a), " + usuarioLogado.getNomeCompleto() + "!");
+                    } else {
+                        System.out.println(" Email ou senha inválidos!");
+                    }
+                break;
+                case "0":
+                    rodando = false;
+                default:
+                    System.out.println(" Opção inválida! Digite 0, 1 ou 2.");
+                    break;
+            }
+
+        } while (rodando);
+
+        sc.close();
     }
-    System.out.println("Escolha uma poção: ");
-    String opcao = scanner.nextLine().trim();
-    switch (opcao) {
-        case "1":
-            System.out.println("Acervo");
-            break;
-        case "0":
-            rodando = false;
-            System.out.println("Até a próxima!");
-            break;
-        default:
-            System.out.println("Opção inválida");
-            break;
-    }
-   }
 }
--- src/Usuario.java
+++ src/Usuario.java
@@ -1,27 +1,43 @@
 public class Usuario {
     private String nome;
-    private String cpf;
-    private String telefone;
+    private String sobrenome;
     private String email;
+    private String senha;
+    private String id;
+    private String nomeCompleto;
+    private static int contador = 0;
 
-    public Usuario(String nome, String cpf, String telefone, String email){
+    public Usuario(String nome, String nomeCompleto, String email, String senha, String sobrenome) {
         this.nome = nome;
-        this.cpf =  cpf;
-        this.telefone = telefone;
+        this.sobrenome = sobrenome;
         this.email = email;
+        this.senha = senha;
+        this.nomeCompleto = nomeCompleto;
+        this.id = "USR" + contador++;
     }
 
+     public boolean verificarSenha(String senhaDigitada) {
+        return this.senha.equals(senhaDigitada);
+    }
     public String getNome() {
         return nome;
     }
 
-    public String getCpf() {
-        return cpf;
-    }
-    public String getTelefone() {
-        return telefone;
-    }
-    public String getEmail(){
+    public String getEmail() {
         return email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+    public String getId(){
+        return id;
+    }
+    public String getNomeCompleto(){ 
+        return nome + " " + sobrenome;
     }
 }
